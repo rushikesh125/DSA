572e7d121f243a60e006fea454fc6eb3e9749f5a
Rushikesh Gaikwad <104708113+rushikesh125@users.noreply.github.com>
2026-05-08 23:24:06 +0530

    feat(leetcode): solve Remove Nth Node From End of List [C++]

--- LeetCode/Medium/Remove-Nth-Node-From-End-of-List.cpp
+++ LeetCode/Medium/Remove-Nth-Node-From-End-of-List.cpp
@@ -1,32 +1,31 @@
 # Remove Nth Node From End of List
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+- URL: https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/1998325484/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: N/A
-- Solved At: 2026-05-08T17:46:16.355Z
+- Memory: 19.32
+MB
+- Solved At: 2026-05-08T17:54:07.310Z
 
 ## Code
 ```cpp
-slow = head
-        prev = None
-        
-        while fast is not None :
-            prev = slow
-            fast = fast.next
-            slow = slow.next
-        if prev is None:
-            return head.next
-            counter+=1
-            fast = fast.next
-        while  counter <n:
-        counter = 0
-        fast = head
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-class Solution:
-#         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
 #         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast  = head 
+        counter = 0
+        while counter <n:
+            fast = fast.next
+            counter +=1
+        
+        prev = None
+        slow = head
+        while fast is not None:
 ```
