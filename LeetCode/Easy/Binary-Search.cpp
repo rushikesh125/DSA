2ce5683faadf9497c9376b4104fdd3eada4c22cd
Rushikesh Gaikwad <104708113+rushikesh125@users.noreply.github.com>
2026-05-09 15:33:16 +0530

    feat(leetcode): solve Binary Search [C++]

--- LeetCode/Easy/Binary-Search.cpp
+++ LeetCode/Easy/Binary-Search.cpp
@@ -1,31 +1,30 @@
 # Binary Search
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/binary-search/submissions/1998717983/
+- URL: https://leetcode.com/problems/binary-search/submissions/1998722683/
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
 - Memory: 20.46
 MB
-- Solved At: 2026-05-09T09:55:07.290Z
+- Solved At: 2026-05-09T10:03:16.451Z
 
 ## Code
 ```cpp
 class Solution:
-    def search(self, nums: List[int], target: 
-int) -> int:
-        n = len(nums)-1
-        start = 0
-        end = n
-        while start <=end:
-            mid = (start+end)//2
-            if(nums[mid]==target):
-                return mid
-            elif nums[mid]<target:
-                start = mid+1
-            else:
-                end = mid-1
-            
-        return -1
+    def binSearch(self,nums,start,end,target)
+:
+        if start>end:
+            return -1
+        mid  = (start+end)//2
+        if(nums[mid]==target):
+            return mid
+        elif nums[mid]<target:
+            return self.binSearch(nums,mid+1,
+end,target)
+        else:
+            return self.binSearch(nums,start,
+mid-1,target)
+    def search(self, nums: List[int],
 ```
