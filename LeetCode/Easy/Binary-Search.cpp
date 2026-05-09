f0726a74f72634dbd316233b5429e53e987e0f26
Rushikesh Gaikwad <104708113+rushikesh125@users.noreply.github.com>
2026-05-09 15:25:07 +0530

    feat(leetcode): solve Binary Search [C++]

--- LeetCode/Easy/Binary-Search.cpp
+++ LeetCode/Easy/Binary-Search.cpp
@@ -1,28 +1,31 @@
 # Binary Search
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/binary-search/
+- URL: https://leetcode.com/problems/binary-search/submissions/1998717983/
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: N/A
-- Solved At: 2026-05-09T09:28:19.389Z
+- Memory: 20.46
+MB
+- Solved At: 2026-05-09T09:55:07.290Z
 
 ## Code
 ```cpp
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        n=len(nums)-1
-·‌·‌·‌·‌·‌·‌·‌·‌start·‌=·‌0
-·‌·‌·‌·‌·‌·‌·‌·‌end·‌=·‌n-1
-·‌·‌·‌·‌·‌·‌·‌·‌while·‌start·‌<=·‌end:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌mid=(start+end)//2
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if(nums[mid]·‌==·‌target):
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌mid
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌elif·‌nums[mid]<target:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌start·‌=·‌mid+1
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌else:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌end·‌=·‌mid-1
-·‌·‌·‌·‌·‌·‌·‌·‌return·‌-1
+    def search(self, nums: List[int], target: 
+int) -> int:
+        n = len(nums)-1
+        start = 0
+        end = n
+        while start <=end:
+            mid = (start+end)//2
+            if(nums[mid]==target):
+                return mid
+            elif nums[mid]<target:
+                start = mid+1
+            else:
+                end = mid-1
+            
+        return -1
 ```
